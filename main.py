62dcb861a4f9eb321763f857ed3f8d8cfa13886c
kristian roopnarine <kristian.roopnarine@gmail.com>
2025-01-14 20:56:16 -0500

    create pygame groups

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill("black")
@@ -14,7 +17,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         player.update(dt)
-        player.draw(screen)
+        for u in updatable:
+            u.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -41,4 +41,3 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-        print(self.position)
